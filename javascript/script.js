0a4f61ddee1b1df479548767bbd5a4487a67803c
glarson175 <gsl13@pitt.edu>
2016-10-13 15:19:42 -0400

    I think I am done
    that was fun. added a table of pictures for my pictures page. added
    contact info. changed colors and whatnot

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
-	//document.getElementById("home").style.visibility = "hidden";
 	Home();
-	
 });
 function Home() {
 	$('#home').show();
 	$('#contact').hide();
+	$('#coolpics').hide();
   return false;
+}
+function Gallery(){
+	$('#home').hide();
+	$('#contact').hide();
+	$('#coolpics').show();
+	return false;
 }
 function Contact() {
 	$('#home').hide();
 	$('#contact').show();
+	$('#coolpics').hide();
+	$('.messagesent').hide();
   return false;
 }
+$('#form').submit(function () {
+		if(document.getElementById('firstname').attributes)
+		$('.text').hide();
+		$('.messagesent').show();
+		$('.submit').hide();
+		return false;
+});
